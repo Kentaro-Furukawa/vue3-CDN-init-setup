5a3dea8829e9ba2e2e618139c7a45e8d2b398784
Ken <info@ken89.com>
2022-05-14 21:16:58 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    template:
+    /*html*/
+    `<h1>Hello Vue3 {{ myName }}</h1>`,
+    data() {
+        return {
+            myName: 'Ken'
+        }
+    }
+
+
+})
+
+app.mount('#app')
